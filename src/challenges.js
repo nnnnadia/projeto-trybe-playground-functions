b2af155afe9077918458a3d963173fedaf06bece
Nádia <tristaonadia@gmail.com>
2022-02-08 15:40:49 -0300

    Requisito 3 feito.

--- src/challenges.js
+++ src/challenges.js
@@ -10,8 +10,9 @@ function calcArea(base, height) {
 }
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(sentence) {
+  let sentenceArray = sentence.split(' ');
+  return sentenceArray;
 }
 
 // Desafio 4
